e691020a1c039239b023b48705f21f6afacca4a2
Kwesi Dadson <samson.dadson@gmail.com>
2022-08-02 09:02:56 +0000

    heap sort

--- functions/heap.go
+++ functions/heap.go
@@ -7,9 +7,44 @@ type HeapItem struct {
 }
 
 func GetHeapItem(arr []int, index int) HeapItem {
+
 	return HeapItem{
-		parentIndex: arr[index/2],
-		leftIndex: arr[2*index],
-		rightIndex: arr[2*index + 1],
+		parentIndex: index/2,
+		leftIndex: 2*index,
+		rightIndex: 2*index + 1,
+	}
+}
+
+func MaxHeapify(arr []int, i int) {
+	l := 2*i
+	r := 2*i + 1
+	n := len(arr) - 1
+
+	var largest int;
+
+	if l <= n && arr[l] > arr[i] {
+		largest = l
+	}else{
+		largest = i
+	}
+
+	if r <= n && arr[r] > arr[largest]{
+		largest = r
+	}
+
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		MaxHeapify(arr, largest)
+	}
+}
+
+func BuildMaxHeap(arr []int){
+	n := len(arr) - 1
+	//the elements in the subarray arr[n/2 + 1 : n] are all leaves of the tree and so each
+	//is a 1-element heap to begin with. We go through the remaining of the array from
+	//last to first
+	
+	for i := n/2; i >= 0; i-- {
+		MaxHeapify(arr, i)
 	}
 }
--- main.go
+++ main.go
@@ -10,8 +10,15 @@ import (
 func main(){
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 
+
+	//heap
 	a := []int{ 1,2,3,4,5,6,7,8,9}
 	b := functions.GetHeapItem(a, 1)
 
 	fmt.Println(b)
+
+	hp := []int{4,1,3,2,16,9,10,14,8,7}
+	functions.BuildMaxHeap(hp)
+
+	fmt.Println(hp)
 }
